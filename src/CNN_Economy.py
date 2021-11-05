da5186da4ff79f1272db284bf7655bc45a4df397
Vcvc <vcvckw@gmail.com>
2021-11-05 10:05:12 +0800

    cnn

---
+++ src/CNN_Economy.py
@@ -0,0 +1,129 @@
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import load_workbook, Workbook
+import json
+import time
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+}
+
+class CNN(object):
+    def __init__(self):
+        pass
+
+    def request(self):
+        self.url = 'https://edition.cnn.com/business/'
+        for _ in range(10):
+            try:
+                self.data = requests.get(self.url, headers=headers)
+                self.soup = BeautifulSoup(self.data.text, "lxml")
+                break
+            except:
+                continue
+
+    def request2(self):
+        #self.en_url = 'https://edition.cnn.com/business/economy'
+        self.en_url = 'https://edition.cnn.com/data/ocs/container/coverageContainer_7FCD2EF6-BE29-BE10-44E3-34EB048CF28E:list-xs/views/containers/common/container-manager.html'
+        for _ in range(10):
+            try:
+                self.en_data = requests.get(self.en_url, headers=headers)
+                self.en_soup = BeautifulSoup(self.en_data.text, "lxml")
+                break
+            except:
+                continue
+
+    def get_business(self):
+        wb = load_workbook(self.xlsxname)
+        sheet = wb.get_sheet_by_name('Cnn')
+        t_row = 1
+        t_col = 1
+
+        sheet.cell(row=t_row + 0, column=t_col + 0, value="CNN商业")
+        sheet.cell(row=t_row + 1, column=t_col + 0, value="新闻标题")
+        sheet.cell(row=t_row + 1, column=t_col + 1, value="新闻链接")
+        sheet.cell(row=t_row + 1, column=t_col + 2, value="新闻简介")
+        sheet.cell(row=t_row + 1, column=t_col + 3, value="新闻时间")
+        t_row = t_row + 2
+
+        datalist = self.soup.select('#us-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul')
+        for data in datalist:
+            news = data.find_all('a')
+            for m_new in news:
+                m_href = "https://edition.cnn.com" + m_new['href']
+                m_title = m_new.get_text()
+                if m_title == '':
+                    continue
+                sheet.cell(row=t_row, column=t_col, value=m_title)
+                sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+                t_row = t_row + 1
+
+        datalist2 = self.soup.select('#us-zone-1 > div.l-container > div > div.column.zn__column--idx-1 > ul')
+        for data in datalist2:
+            news = data.find_all('a')
+            for m_new in news:
+                m_href = "https://edition.cnn.com" + m_new['href']
+                m_title = m_new.get_text()
+                if m_title == '':
+                    continue
+                sheet.cell(row=t_row, column=t_col, value=m_title)
+                sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+                t_row = t_row + 1
+
+        try:
+            wb.save(self.xlsxname)
+        except Exception:
+            print("CNN Save Error = 1")
+
+
+    def create_file(self):
+        wb = Workbook()
+        ws = wb['Sheet']
+        wb.remove(ws)
+        sheet = wb.create_sheet("Cnn")
+
+        try:
+            wb.save(self.xlsxname)
+        except Exception:
+            print("CNN create_error = 1")
+
+    def get_economy(self):
+        wb = load_workbook(self.xlsxname)
+        sheet = wb.get_sheet_by_name("Cnn")
+        t_row = sheet.max_row + 2
+        t_col = 1
+        sheet.cell(row=t_row, column=t_col, value="CNN经济")
+        t_row = t_row + 1
+        sheet.cell(row=t_row, column=t_col, value="新闻标题")
+        sheet.cell(row=t_row, column=t_col + 1, value="新闻链接")
+        sheet.cell(row=t_row, column=t_col + 2, value="新闻简介")
+        sheet.cell(row=t_row, column=t_col + 3, value="新闻时间")
+        t_row = t_row + 1
+
+        datalist2 = self.en_soup.select('#coverageContainer_7FCD2EF6-BE29-BE10-44E3-34EB048CF28E > ul')
+        for data in datalist2:
+            news = data.find_all('a')
+            for m_new in news:
+                m_href = "https://edition.cnn.com" + m_new['href']
+                m_title = m_new.get_text()
+                if m_title == '':
+                    continue
+                sheet.cell(row=t_row, column=t_col, value=m_title)
+                sheet.cell(row=t_row, column=t_col + 1, value=m_href)
+                t_row = t_row + 1
+
+        try:
+            wb.save(self.xlsxname)
+        except Exception:
+            print("CNN Save Error = 2")
+
+    def main(self, file_name):
+        self.xlsxname = file_name
+        Cnn.create_file()
+        Cnn.request()
+        Cnn.get_business()
+        Cnn.request2()
+        Cnn.get_economy()
+
+
+Cnn = CNN()
---
+++ src/Platform.py
@@ -0,0 +1,123 @@
+import platform
+import os
+import shutil
+import time
+
+class Files(object):
+    def __init__(self):
+       pass
+
+    def get_platform(self):
+        sys = platform.system()
+        if sys == "Windows":
+            return True
+        elif sys == "Linux":
+            return False
+
+    def file_move(self, platform):
+        if platform:
+            self.win_News()
+        else:
+            self.lin_News()
+
+    def lin_News(self):
+        path = "./Finance/News/"
+        filename = "./News_Economy.xlsx"
+        isExists = os.path.exists(path)
+        if not isExists:
+            os.mkdir(path)
+        #filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime())  # year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d_%H", time.localtime())  # year-month-day-hour-minute
+        path2 = path + "闻讯E__" + filetime + "时.xlsx"
+        shutil.move(filename, path2)
+
+
+    def win_News(self):
+        desktop_path = os.path.join(os.path.expanduser('~'),"Desktop") #获取桌面路径
+        filename = desktop_path + "\\Finance\\News_Economy.xlsx"
+        path = desktop_path +"\\Finance\\News\\"
+
+        isExists = os.path.exists(path)
+        if not isExists:
+            os.mkdir(path)
+        #filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime()) #year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d_%H", time.localtime()) #year-month-day-hour-minute
+        path2 = path + "闻讯E__" + filetime + "时.xlsx"
+        shutil.move(filename, path2)
+
+    def linux_filename(self):
+        path = "./Finance/"
+        isExists = os.path.exists(path)
+        if not isExists:
+            os.mkdir(path)
+        # self.lin_History()
+        linux_file = "./News_Economy.xlsx"
+        return linux_file
+
+    def win_filename(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        path = desktop_path + "\\Finance\\"
+        isExists = os.path.exists(path)
+        if not isExists:
+            os.mkdir(path)
+
+        win_file = desktop_path + "\\Finance\\News_Economy.xlsx"
+        return win_file
+
+    def pause(self):
+        os.system('pause')
+
+    """
+    def win_mkdir(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        dir = os.path.exists(desktop_path + "\\Finance")
+        if not dir:
+            os.mkdir(dir)
+    """
+    def getwinfile(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        filename = desktop_path + "\\Finance\\News_Economy.xlsx"
+        path = desktop_path + "\\Finance\\News\\"
+        # filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime()) #year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = path + "闻讯E__" + filetime + ".xlsx"
+        return path2
+
+    def getlinfile(self):
+        path = "./Finance/News/"
+        filename = "./News_Economy.xlsx"
+        isExists = os.path.exists(path)
+        # filetime = time.strftime("%Y_%m_%d_%H_%M", time.localtime())  # year-month-day-hour-minute
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = path + "闻讯E__" + filetime + ".xlsx"
+        return path2
+
+
+    def getfilename(self):
+        if self.get_platform() == True:
+            return self.getwinfile()
+        else:
+            return self.getlinfile()
+
+    def filename(self):
+        filetime = time.strftime("%Y_%m_%d", time.localtime())  # year-month-day-hour-minute
+        path2 = "闻讯E__" + filetime + ".xlsx"
+        return path2
+
+    def getwinpath(self):
+        desktop_path = os.path.join(os.path.expanduser('~'), "Desktop")  # 获取桌面路径
+        path = desktop_path + "\\Finance\\" + " \\"
+        return path
+
+    def getlinpath(self):
+        path = "./Finance/" + " /"
+        return path
+
+    def getpath(self):
+        if self.get_platform() == True:
+            return self.getwinpath()
+        else:
+            return self.getlinpath()
+
+
+pt = Files()
