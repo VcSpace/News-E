e85a1ac88a1f18a1e25af0cbe90be2baaa6c7f27
Vcvc <vcvckw@gmail.com>
2021-11-05 10:05:18 +0800

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+import os
+
+from src.Platform import pt
+from src.CNN_Economy import Cnn
+
+def get_News(platform, filename, debug):
+    #debug True开启
+    if debug:
+        Cnn.create_file(filename)
+        return
+    Cnn.main(filename)
+
+def get_filename(platform):
+    if platform == True:
+        win_file = pt.win_filename()
+        return win_file
+    else:
+        linux_file = pt.linux_filename()
+        return linux_file
+
+if __name__ == '__main__':
+    Debug = False
+    m_platform = pt.get_platform() #判断系统
+    filename = get_filename(m_platform)
+    get_News(m_platform, filename, Debug) #获取信息
+
+    pt.file_move(m_platform) #文件移动 重命名操作
+
+    if m_platform == True:
+        pt.pause()
